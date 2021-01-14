d6b107e12c2411e624df7a8357b55ab481a22440
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-13 17:13:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+const url = 'http://localhost:3000/users'
+
+function submitData(name, email) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({ 'name': name, 'email': email })
+      };
+
+    return fetch(url, configObj)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(body) {
+            const p = document.createElement('p');
+            p.innerHTML = body.id;
+            document.body.appendChild(p);
+        })
+        .catch(function(error) {
+        alert("Error:");
+            const p = document.createElement('p');
+            p.innerHTML = error.message;
+            document.body.appendChild(p);
+        });
+}
